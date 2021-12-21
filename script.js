33a2190fa70111fcb5c3d8e9c95457bccc4e92e6
Nikita <inikets1@gmail.com>
2021-12-21 19:56:32 +0300

    4c

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function testStrictNotEqual(val) {
 
   console.log(Tasks)
   console.log(12)
+
+
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+
+while (i >= 0){
+  myArray.push(i);
+  i--;
+}
